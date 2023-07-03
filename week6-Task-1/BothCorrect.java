3712380cebfe67ce8200387563cc803405a05d68
unknown <Arunkumar11418>
2023-07-03 17:33:52 +0530

    first commit

---
+++ week6-Task-1/BothCorrect.java
@@ -0,0 +1,38 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+@Test
+public class BothCorrect {
+	
+	public void loginwithbothcorrect() throws InterruptedException{
+	
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\SSLTP11460\\eclipse-workspace\\selenium\\driver\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
+	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+	Thread.sleep(2000);
+	
+	//username
+	WebElement username =driver.findElement(By.name("username"));
+	username.sendKeys("Admin");
+	Thread.sleep(2000);
+	
+	//password
+	WebElement password =driver.findElement(By.xpath("//input[@name='password']"));
+	password.sendKeys("admin123");
+	
+	//login
+	WebElement loginbutton =driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button"));
+	loginbutton.click();
+	Thread.sleep(3000);
+	
+	
+	driver.quit();
+	
+	}
+}
